66992e401f2f28c19687679495b4e3325c6fff8c
frontend-antony <elmer.anthony12@gmail.com>
2023-03-19 01:43:41 -0500

    subiendo

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src="https://brother-antony.github.io/PORTAFOLIO/img/logo.png" alt="" />
+      <span>Made with ♥️ and <b>React.js</b>.</span>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Register = () => {
+  return (
+    <div className='auth'>
+      <h1>Register</h1>
+      <form action="">
+        <input type="text" placeholder='username' required />
+        <input type="email" placeholder='email' required />
+        <input type="password" placeholder='password' required />
+        <button>Register</button>
+        <p>This is an error!</p>
+        <span>Do you have an account? <Link to="/login">Login</Link></span>
+      </form>
+    </div>
+  )
+}
+
+export default Register
